ebf906c0c8f0e95a30e9f11b64a9bba65f94e038
mewannirundakka-hub <ict22001@std.uwu.ac.lk>
2026-03-21 15:33:11 +0530

    initial project buid with db

---
+++ src/test/java/com/mewan/smart_grocery_finder/SmartGroceryFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mewan.smart_grocery_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartGroceryFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
